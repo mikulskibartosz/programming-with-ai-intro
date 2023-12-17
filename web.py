577ef23d1c946e4abed214366813bbc08bca27f0
Bartosz Mikulski <mail@mikulskibartosz.name>
2023-12-17 11:18:25 +0100

    web app

--- web.py
+++ web.py
@@ -2,10 +2,10 @@ from flask import Flask, jsonify
 from rest.service import Service
 
 app = Flask(__name__)
+service = Service()
 
 @app.route('/users', methods=['GET'])
 def get_all_users():
-    service = Service()
     users = service.get_all_users()
     return jsonify(users)
 
